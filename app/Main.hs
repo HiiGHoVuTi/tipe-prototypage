800532e12b838b7ad529b587730ad08ebb94b369
Maxime <emixampons@gmail.com>
2023-08-09 18:33:34 +0200

    🐛: evaluation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.DeepSeq
+import Control.Monad
 import Data.Bitraversable
 import Data.Data
 import Data.Foldable
@@ -49,7 +50,7 @@ data Node
   | Duplicated NodeRef
   | IntegerValue Int
   | Lambda NodeRef NodeRef
-  | Variable
+  | Variable (Maybe NodeRef)
   | Application NodeRef NodeRef
   | Constructor Identifier [NodeRef]
   deriving (Eq, Generic, Data, NFData)
@@ -59,8 +60,8 @@ showNode = show . toConstr
 
 createDup :: Identifier -> NodeRef -> STM (NodeRef, NodeRef, NodeRef)
 createDup ι α = do
-  δ₁ <- newNodeRef Variable
-  δ₂ <- newNodeRef Variable
+  δ₁ <- newNodeRef (Variable Nothing)
+  δ₂ <- newNodeRef (Variable Nothing)
   ρ <- newNodeRef (Duplication ι α (δ₁, δ₂))
   writeNodeRef δ₁ (Duplicated ρ)
   writeNodeRef δ₂ (Duplicated ρ)
@@ -73,78 +74,88 @@ duplicationOf ν = do
   atomically do createDup ι α
 
 -- TODO(Maxime): verify whether you need different identifiers
-nDuplicates :: Identifier -> Nat -> NodeRef -> STM [NodeRef]
-nDuplicates _ 0 ____ = pure []
-nDuplicates _ 1 node = pure [node]
-nDuplicates ι n node = do
-  nDuplicates ι (n - 1) node >>= \case
-    [] -> error "impossible"
-    (latestClone : rest) -> do
-      (_, δ₁, δ₂) <- createDup ι latestClone
-      pure (δ₁ : δ₂ : rest)
+nDuplicates :: Nat -> NodeRef -> IO [NodeRef]
+nDuplicates 0 ____ = pure []
+nDuplicates 1 node = pure [node]
+nDuplicates n node = do
+  ι <- randomIO
+  (latestClone : rest) <- nDuplicates (n - 1) node
+  (_, δ₁, δ₂) <- atomically do createDup ι latestClone
+  pure (δ₁ : δ₂ : rest)
 
 lambdaHelper :: (NodeRef -> STM NodeRef) -> STM NodeRef
 lambdaHelper body = do
-  α <- newNodeRef Variable
+  α <- newNodeRef (Variable Nothing)
   ν <- body α
   newNodeRef (Lambda α ν)
 
 evaluate :: NodeRef -> IO Node
 evaluate root =
   readNodeRefIO root >>= \case
+    Variable (Just α) -> evaluate α
     Duplicated ρ -> do
       Duplication ι v (δ₁, δ₂) <- readNodeRefIO ρ
-      readNodeRefIO v >>= \case
+      -- NOTE(Maxime): avoid extra work, evaluate once
+      β <- evaluate v
+      atomically do writeNodeRef v β
+      unless (root `elem` [δ₁, δ₂]) (error "INCOHERENT")
+      case β of
         Constructor μ xs ->
           atomically do
             (_, δ₁s, δ₂s) <- unzip3 <$> traverse (createDup ι) xs
             writeNodeRef δ₁ (Constructor μ δ₁s)
             writeNodeRef δ₂ (Constructor μ δ₂s)
-            *> evaluate δ₁
+            *> evaluate root
         Lambda arg body ->
           atomically do
-            arg'₁ <- newNodeRef Variable
-            arg'₂ <- newNodeRef Variable
+            arg'₁ <- newNodeRef (Variable Nothing)
+            arg'₂ <- newNodeRef (Variable Nothing)
             (_, body'₁, body'₂) <- createDup ι body
-            writeNodeRef arg (Superposition ι (arg'₁, arg'₂))
+            σ <- newNodeRef (Superposition ι (arg'₁, arg'₂))
+            writeNodeRef arg (Variable (Just σ))
             writeNodeRef δ₁ (Lambda arg'₁ body'₁)
             writeNodeRef δ₂ (Lambda arg'₂ body'₂)
-            *> evaluate δ₁
+            *> evaluate root
         Superposition ι' (σ₁, σ₂)
-          | ι == ι' ->
+          | ι == ι' -> do
               atomically do
                 writeNodeRef δ₁ =<< readNodeRef σ₁
                 writeNodeRef δ₂ =<< readNodeRef σ₂
-                *> evaluate δ₁
+                *> evaluate root
           | otherwise -> do
+              putStrLn "ohno" :: IO ()
               (ι₁, ι₂) <- bisequence (randomIO, randomIO)
               atomically do
                 (_, δ₁σ₁, δ₂σ₁) <- createDup ι₁ σ₁
                 (_, δ₁σ₂, δ₂σ₂) <- createDup ι₂ σ₂
                 writeNodeRef δ₁ (Superposition ι₁ (δ₁σ₁, δ₁σ₂))
                 writeNodeRef δ₂ (Superposition ι₂ (δ₂σ₁, δ₂σ₂))
-              evaluate δ₁
-        n ->
+              evaluate root
+        n@IntegerValue {} -> do
           atomically do
             writeNodeRef δ₁ n
             writeNodeRef δ₂ n
-            *> evaluate δ₁
-    -- _n -> error "invariant broken"
+            *> evaluate root
+        -- NOTE(Maxime): already in nf
+        _ -> error "invariant broken"
     Application φ α ->
       readNodeRefIO φ >>= \case
         Lambda arg body ->
           atomically do
-            writeNodeRef arg =<< readNodeRef α
+            writeNodeRef arg (Variable (Just α))
             *> evaluate body
         Superposition ι (σ₁, σ₂) ->
           atomically do
-            writeNodeRef σ₁ (Application φ σ₁)
-            writeNodeRef σ₂ (Application φ σ₂)
-            writeNodeRef root (Superposition ι (σ₁, σ₂))
+            (_, α₁, α₂) <- createDup ι α
+            σ₁' <- newNodeRef (Application σ₁ α₁)
+            σ₂' <- newNodeRef (Application σ₂ α₂)
+            writeNodeRef root (Superposition ι (σ₁', σ₂'))
             *> evaluate root
-        _ -> do
+        f -> do
           ψ <- evaluate φ
           atomically do writeNodeRef φ ψ
+          -- TODO(Maxime): avoid this using a whnf detector
+          when (f == ψ) (error ("impossible to evaluate " <> showNode f))
           evaluate root
     node -> pure node
 
@@ -156,50 +167,69 @@ prop_vie_est_belle = True
 prop_id_on_int :: Int -> Property
 prop_id_on_int i = monadicIO $ run do
   let expected = IntegerValue i
-  valu <- newNodeRefIO expected
-  ldva <- newNodeRefIO Variable
-  lmbd <- newNodeRefIO (Lambda ldva ldva)
-  root <- newNodeRefIO (Application lmbd valu)
+  value <- newNodeRefIO expected
+  lambda <- atomically do lambdaHelper pure
+  root <- newNodeRefIO (Application lambda value)
   result <- evaluate root
   pure (result == expected)
 
 prop_dup_id :: Int -> Property
 prop_dup_id i = monadicIO $ run do
   let expected = IntegerValue i
-  inpu <- newNodeRefIO expected
-  ldva <- newNodeRefIO Variable
-  lmid <- newNodeRefIO (Lambda ldva ldva)
-  (_, clo1, _) <- atomically do createDup 0 lmid
-  root <- newNodeRefIO (Application clo1 inpu)
+  input <- newNodeRefIO expected
+  lambda <- atomically do lambdaHelper pure
+  (_, clone1, _) <- atomically do createDup 0 lambda
+  root <- newNodeRefIO (Application clone1 input)
   result <- evaluate root
   pure (result == expected)
 
+prop_dup_cons :: Word -> Property
+prop_dup_cons i = monadicIO $ run do
+  lab <- randomIO
+  let expected = Constructor i []
+  input <- newNodeRefIO expected
+  (_, out1, out2) <- atomically do createDup lab input
+  res1 <- evaluate out1
+  res2 <- evaluate out2
+  pure (expected == res1 && expected == res2)
+
+prop_not :: NodeRef -> Int -> IO Bool
+prop_not f p = do
+  dummie1 <- newNodeRefIO (IntegerValue 0)
+  dummie2 <- newNodeRefIO (IntegerValue 1)
+  partial <- newNodeRefIO (Application f dummie1)
+  root <- newNodeRefIO (Application partial dummie2)
+  result <- evaluate root
+  pure (result == IntegerValue p)
+
 prop_not_composition_naive :: Nat -> IO Node
 prop_not_composition_naive n = do
-  i <- randomIO
   true <- atomically do lambdaHelper \t -> lambdaHelper \_ -> pure t
   notF <- atomically do
-    lambdaHelper \t -> lambdaHelper \f -> lambdaHelper \p -> do
+    lambdaHelper \p -> lambdaHelper \t -> lambdaHelper \f -> do
       partial <- newNodeRef (Application p f)
       newNodeRef (Application partial t)
-  nots <- atomically do nDuplicates i n notF
-  result <- atomically do foldlM ((newNodeRef .) . Application) true nots
+  nots <- nDuplicates n notF
+  result <- atomically do foldlM ((newNodeRef .) . flip Application) true nots
   evaluate result
 
 prop_not_composition :: Nat -> IO Node
 prop_not_composition n = do
-  i <- randomIO
   true <- atomically do lambdaHelper \t -> lambdaHelper \_ -> pure t
   notF <- atomically do
-    lambdaHelper \t -> lambdaHelper \f -> lambdaHelper \p -> do
+    lambdaHelper \p -> lambdaHelper \t -> lambdaHelper \f -> do
       partial <- newNodeRef (Application p f)
       newNodeRef (Application partial t)
-  ff <- atomically do
-    lambdaHelper \f -> lambdaHelper \x -> do
-      partial <- newNodeRef (Application f x)
-      newNodeRef (Application f partial)
-  ffs <- atomically do nDuplicates i n ff
-  finalF <- atomically do foldlM ((newNodeRef .) . Application) notF ffs
+  let mkff = do
+        i <- randomIO
+        atomically do
+          lambdaHelper \f -> lambdaHelper \x -> do
+            (_, f1, f2) <- createDup i f
+            partial <- newNodeRef (Application f1 x)
+            newNodeRef (Application f2 partial)
+  -- forced to cheat here, TODO(Maxime): use constructors to solve this
+  ffs <- replicateM (fromEnum n) mkff
+  finalF <- atomically do foldlM ((newNodeRef .) . flip Application) notF ffs
   result <- newNodeRefIO (Application finalF true)
   evaluate result
 
@@ -208,21 +238,28 @@ main =
   defaultMain
     [ testProperty "tautology" prop_vie_est_belle,
       bgroup
-        "correctness"
+        "interpreter correctness"
         [ testProperty "identity" prop_id_on_int,
           bgroup
             "duplication"
-            [ testProperty "duplication of the identity" prop_dup_id
+            [ testProperty "duplication of the identity" prop_dup_id,
+              testProperty "duplication of a constructor" prop_dup_cons
             ]
         ],
       bgroup
         "boolean not (Church)"
-        [ testProperties
+        [ testProperty
             "not composition correctness"
-            [],
+            $ property \n -> n >= 0 ==> monadicIO $ run do
+              newF <- newNodeRefIO =<< prop_not_composition_naive (toEnum n)
+              prop_not newF (n `mod` 2),
+          testProperty
+            "not 2^n composition correctness"
+            $ property \n -> n >= 0 ==> monadicIO $ run do
+              newF <- newNodeRefIO =<< prop_not_composition (toEnum n)
+              prop_not newF (fromEnum (n == 0)),
           bgroup
             "no fusion"
-            -- TODO
             do
               i <- [0, 2 :: Int .. 9]
               let n = 2 ^ i
@@ -233,6 +270,6 @@ main =
             "fusion"
             do
               i <- [0, 2 .. 64]
-              pure $ bench (show @Nat (2^i)) $ nfAppIO prop_not_composition i
+              pure $ bench (show @Nat (2 ^ i)) $ nfAppIO prop_not_composition i
         ]
     ]
